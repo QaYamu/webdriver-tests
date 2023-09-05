21989e7e2c447e03f472c00d21199725d8dc59d3
vinodhinichandrakasan <105554381+vinodhinichandrakasan@users.noreply.github.com>
2023-09-05 07:05:54 +0200

    Update selenium_script.sh

--- selenium_script.sh
+++ selenium_script.sh
@@ -2,11 +2,10 @@
 
 #maven cmd to run the automated script
 echo "Going to run the automated script"
-        cd ~/webdriver-tests
-        mvn clean test
+       mvn clean test
 #command to aws configure sync
 echo "Have run the script and now going to sync the report"
-        cd ~/webdriver-tests/
+       
         ls -la
         sleep 3
         echo "present directory is" $(pwd)
