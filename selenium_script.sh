3d807d1ecf4a98859cecc8e349ffe0c8d0ea4c81
Yamuna <jayyamuna18@gmail.com>
2023-09-08 18:47:21 +0530

    changed in sh

--- selenium_script.sh
+++ selenium_script.sh
@@ -9,4 +9,4 @@ echo "Have run the script and now going to sync the report"
         ls -la
         sleep 3
         echo "present directory is" $(pwd)
-        aws s3 sync reports/ s3://reports-for-selenium
+        aws s3 sync reports/ s3://ubuntureports
