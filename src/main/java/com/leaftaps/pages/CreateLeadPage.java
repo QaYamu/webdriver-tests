0a16406be05bf01b4aba8cf4957a51fceda46a4c
QaYamu <144373655+QaYamu@users.noreply.github.com>
2023-09-17 11:42:47 +0530

    Update CreateLeadPage.java

--- src/main/java/com/leaftaps/pages/CreateLeadPage.java
+++ src/main/java/com/leaftaps/pages/CreateLeadPage.java
@@ -8,6 +8,7 @@ public class CreateLeadPage extends ProjectHooks{
 	public CreateLeadPage enterCompanyName(String data) {
 		clearAndType(locateElement(Locators.XPATH, "//input[@id='createLeadForm_companyName']"), data);
 		reportStep(data+" company name is entered successfully","pass");
+		System.out.println();
 		return this;
 	}
 	public CreateLeadPage enterFirstName(String data) {
